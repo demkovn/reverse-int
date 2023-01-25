8baf6704db5c3eeb7f1d595656151bcd5f56819f
demkovn <112659532+demkovn@users.noreply.github.com>
2023-01-25 22:48:51 +0200

    fix: update reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+  const str = (Math.abs(n)).toString();
+  let out = '';
+  const isNegative = n < 0;
+  for ( let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (char !== '0') {
+      out = char + out;};
+   else if (char === '0' && str[i+1] && str[i+1] !== '0') {
+   out = char + out;}
+  };
+    return Number(isNegative ? out * 1 : out);
 }
