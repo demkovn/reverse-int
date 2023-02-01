8c076244a2129b2831dfdde30a4f365cf64bfc03
demkovn <112659532+demkovn@users.noreply.github.com>
2023-02-01 18:48:34 +0200

    fix: update undex.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-module.exports = function reverse (n) {
-  const str = (Math.abs(n)).toString();
-  let out = '';
-  const isNegative = n < 0;
-  for ( let i = 0; i < str.length; i++) {
-    const char = str[i];
-    if (char !== '0') {
-      out = char + out;};
-   else if (char === '0' && str[i+1] && str[i+1] !== '0') {
-   out = char + out;}
-  };
-    return Number(isNegative ? out * 1 : out);
-}
+const reverse = (n) => {
+    str = n.toString();
+    out = '';
+          if (str[0] == '-') {
+            out = str.substr(1);
+          } else {
+            out = str;
+          }
+return out.split('').reverse().join('');
+};
+
