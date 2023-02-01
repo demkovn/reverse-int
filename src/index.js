cfdcf070d3f0b366230c44d15930d830d620dd73
demkovn <112659532+demkovn@users.noreply.github.com>
2023-02-01 19:09:40 +0200

    fix: update

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ module.exports = function reverse(n) {
   str = n.toString();
   reversed = '';
         if (str[0] == '-') {
-          out = str.substr(1);
+          reversed = str.substr(1);
         } else {
-          out = str;
+          reversed = str;
         }
 return reversed.split('').reverse().join('');
 };
